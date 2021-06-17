d8480545d424f84b89fa413c58b2bf19d8aee2ac
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-06-17 23:07:51 +0000

    chore: add styled components on mobile

--- Applications/mobile/App.tsx
+++ Applications/mobile/App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform} from 'react-native';
+import styled from 'styled-components/native';
 
+const Text = styled.Text`
+  font-size: 18px;
+  color: blue;
+  font-weight: 500;
+`;
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+`;
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -13,30 +25,11 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Text>Welcome to React Native!</Text>
+        <Text>To get started, edit App.js</Text>
+        <Text>{instructions}</Text>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
