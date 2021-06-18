57d399d41b91c0a20c1b778c7000401a0a2ab63e
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-06-17 22:33:47 -0300

    chore: create Hi component

--- Applications/mobile/App.tsx
+++ Applications/mobile/App.tsx
@@ -1,35 +1,12 @@
 import React from 'react';
-import {Component} from 'react';
-import {Platform} from 'react-native';
 import styled from 'styled-components/native';
-
-const Text = styled.Text`
-  font-size: 18px;
-  color: blue;
-  font-weight: 500;
-`;
-const Container = styled.View`
-  flex: 1;
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-`;
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <Container>
-        <Text>Welcome to pega na minha rola</Text>
-        <Text>To get started, edit App.js</Text>
-        <Text>{instructions}</Text>
-      </Container>
-    );
-  }
+import {ThemeProvider} from 'styled-components';
+import theme from './src/theme';
+import Hi from './src/components/Hi';
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Hi name="ModaFucka!!" />
+    </ThemeProvider>
+  );
 }
