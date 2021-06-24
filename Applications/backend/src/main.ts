3c9e148cf340c899d6215ef8aa7d02c3e43911e8
crislerwin <crisler.silva@alunos.fael.edu.br>
2021-06-24 00:21:58 +0000

    chore: import exception error

--- Applications/backend/src/main.ts
+++ Applications/backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ModelNotFoundExceptionFilter } from '../src/error/model-not-found-exception-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new ModelNotFoundExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
